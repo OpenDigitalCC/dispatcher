3e4be07824c48e3767e16adf935ebb24f88c8d45
SJM <sm@xisl.com>
2026-03-10 15:05:43 +0100

    ``` Enhanced SSL configurations in dispatcher-agent and Engine
    - Updated SSL cipher lists in dispatcher-agent.
    - Added support for new ciphersuites to improve security.
    ```

--- lib/Dispatcher/Engine.pm
+++ lib/Dispatcher/Engine.pm
@@ -469,15 +469,17 @@ sub _build_ua {
     # to IO::Socket::SSL for client cert presentation. Set them as global
     # SSL defaults so every outbound connection uses the dispatcher cert.
     IO::Socket::SSL::set_defaults(
-        SSL_cert_file   => $config->{cert},
-        SSL_key_file    => $config->{key},
-        SSL_ca_file     => $config->{ca},
-        SSL_verify_mode => 0x01,
-        SSL_version     => 'TLSv1_3:TLSv1_2',
-        SSL_cipher_list => 'ECDHE-ECDSA-AES256-GCM-SHA384:'
-                         . 'ECDHE-RSA-AES256-GCM-SHA384:'
-                         . 'ECDHE-ECDSA-CHACHA20-POLY1305:'
-                         . 'ECDHE-RSA-CHACHA20-POLY1305',
+        SSL_cert_file    => $config->{cert},
+        SSL_key_file     => $config->{key},
+        SSL_ca_file      => $config->{ca},
+        SSL_verify_mode  => 0x01,
+        SSL_cipher_list  => 'ECDHE-RSA-AES256-GCM-SHA384:'
+                          . 'ECDHE-ECDSA-AES256-GCM-SHA384:'
+                          . 'ECDHE-RSA-AES128-GCM-SHA256:'
+                          . 'ECDHE-ECDSA-AES128-GCM-SHA256',
+        SSL_ciphersuites => 'TLS_AES_256_GCM_SHA384:'
+                          . 'TLS_CHACHA20_POLY1305_SHA256:'
+                          . 'TLS_AES_128_GCM_SHA256',
     );
 
     my $ua = LWP::UserAgent->new(
